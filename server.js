1093a183ce4b522b1ed6aa6e2f332e7cb9463064
K1EW <weerawatwmt@gmail.com>
2024-03-03 23:06:15 +0700

    connect to mongodb database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const connectDB = require("./config/db")
 
 dotenv.config({ path: "./config/config.env" });
+connectDB();
 const app = express();
 
 app.use(express.json());
