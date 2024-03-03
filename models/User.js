b0c2f237a1662ad76bb58bf77d9a602bf8554ef8
K1EW <weerawatwmt@gmail.com>
2024-03-03 23:58:01 +0700

    remove un-used line from user model

--- models/User.js
+++ models/User.js
@@ -51,5 +51,4 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
 module.exports = mongoose.model('User',userSchema);
