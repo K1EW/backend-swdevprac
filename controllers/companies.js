c51420b40f7b4762777fd23c0de1bc231e251ab4
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2024-03-04 15:36:22 +0700

    test git merge

--- controllers/companies.js
+++ controllers/companies.js
@@ -10,7 +10,7 @@ exports.getCompanies = async (req, res, next) => {
 
         let queryStr = JSON.stringify(req.query);
         queryStr = queryStr.replace(
-            /\b(gt|gte|lt|lte|in)\b/g,
+            /\b(gt|gte|lt|lte|in)\b/g ,
             (match) => `$${match}`
         );
         query = Company.find(JSON.parse(queryStr)).populate("interviews");
@@ -51,41 +51,6 @@ exports.getCompanies = async (req, res, next) => {
                 count: companies.length,
                 pagination,
                 data: companies,
-<<<<<<< HEAD
-              });
-          } catch (err) {
-            res.status(400).json({ success: false });
-          }
-    };
-}
-    
-    exports.getCompany = async (req, res, next) => {
-        try {
-            const company = await Company.findById(req.params.id);
-            if (!company) {
-                return res.status(400).json({ success: false });
-            }
-            res.status(200).json({ success: true, data: company });
-        } catch (error) {
-            res.status(400).json({ success: false });
-        }
-    };
-    
-    exports.createCompany = async (req, res, next) => {
-        const company = await Company.create(req.body);
-        res.status(201).json({
-            success: true,
-            data: company
-        });
-    };
-    
-    exports.updateCompany = async (req, res, next) => {
-        try {
-            const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
-                new: true,
-                runValidators: true
-=======
->>>>>>> origin
             });
     } catch (err) {
         res.status(400).json({ success: false });
