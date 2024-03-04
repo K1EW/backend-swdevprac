a1503349a7427c497691d8a3cebbaec40cfe9426
HRNPH <hrnph@protonmail.com>
2024-03-04 14:00:16 +0700

    interview delete

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -142,7 +142,30 @@ exports.editInterview = async (req, res, next) => {
 
 exports.deleteInterview = async (req, res, next) => {
   try {
+    const interview = await Interviews.findById(req.params.id);
+    if (!interview) {
+      return res.status(404).json({
+        success: false,
+        message: `No Interview with the id of ${req.params.id}`,
+      });
+    }
+
+    // ensure owner if not admin
+    if (req.user.role !== "admin" && interview.user.toString() !== req.user.id) {
+        return res.status(401).json({
+          success: false,
+          message: `User ${req.user.id} is not authorized to delete Interview ${req.params.id}`,
+        });
+    }
+
+    await interview.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: `Interview ${req.params.id} deleted`,
+    });
   } catch (error) {
-    res.status(400).json({ success: false });
+    console.log(error);
+    res.status(400).json({ success: false , message: "Cannot delete Interview" });
   }
 };
