3c3c16189461202665725d2699da75fff01dda3b
HRNPH <hrnph@protonmail.com>
2024-03-04 13:55:37 +0700

    update routes

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -99,7 +99,7 @@ exports.bookInterview = async (req, res, next) => {
 
 exports.editInterview = async (req, res, next) => {
   try {
-    const interview = await Interviews.findById(req.params.id)
+    const interview = await Interviews.findById(req.params.id);
     if (!interview) {
       return res.status(404).json({
         success: false,
@@ -107,8 +107,36 @@ exports.editInterview = async (req, res, next) => {
       });
     }
 
-  } catch (error) {
-    res.status(400).json({ success: false });
+    // ensure owner if not admin
+    if (req.user.role !== "admin" && interview.user.toString() !== req.user.id) {
+        return res.status(401).json({
+          success: false,
+          message: `User ${req.user.id} is not authorized to update Interview ${req.params.id}`,
+        });
+    }
+
+    const updatedInterview = await Interviews.findByIdAndUpdate(
+      req.params.id,
+      {
+        company: req.body.company,
+        date: req.body.date,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: updatedInterview,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot edit Interview, Either the Interview does not exist or the parameters are not correct",
+    });
   }
 };
 
