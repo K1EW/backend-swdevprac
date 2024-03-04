d0c8790ade94e252d25915252b15d1cdfff9c7d5
K1EW <weerawatwmt@gmail.com>
2024-03-04 11:05:09 +0700

    update company controller & route

--- controllers/companies.js
+++ controllers/companies.js
@@ -9,10 +9,53 @@ exports.getCompanies = async (req, res, next) => {
     }
 };
 
+exports.getCompany = async (req, res, next) => {
+    try {
+        const company = await Company.findById(req.params.id);
+        if (!company) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: company });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+};
+
 exports.createCompany = async (req, res, next) => {
     const company = await Company.create(req.body);
     res.status(201).json({
         success: true,
         data: company
     });
+};
+
+exports.updateCompany = async (req, res, next) => {
+    try {
+        const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!company) {
+            return res.status(400).json({ success: false })
+        }
+
+        res.status(200).json({ success: true, data: company });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+};
+
+exports.deleteCompany = async (req, res, next) => {
+    try {
+        const company = await Company.findById(req.params.id);
+
+        if (!company) {
+            return res.status(404).json({ success: false })
+        }
+        await Company.deleteOne();
+        res.status(200).json({ success: true, data: {} });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
--- routes/companies.js
+++ routes/companies.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { protect, authorize } = require("../middleware/auth");
-const { getCompanies, createCompany } = require("../controllers/companies");
+const { getCompanies, createCompany, getCompany, updateCompany, deleteCompany } = require("../controllers/companies");
 
 const router = express.Router();
 
 router.route("/").get(getCompanies).post(createCompany);
+router.route("/:id").get(getCompany).put(updateCompany).delete(deleteCompany);
 
 module.exports = router;
