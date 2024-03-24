00c335b224328ecd1c2d3865a84ae83f6fb8c577
K1EW <weerawatwmt@gmail.com>
2024-03-24 18:26:37 +0700

    Revert "add some salt"
    This reverts commit 40a0871b20be376f7ab12edd0220e1fa4d5a5035.

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,20 +34,20 @@ exports.login = async (req, res, next) => {
                 .status(400)
                 .json({ sucess: false, msg: 'Invalid credentials' });
         }
-
-        const isMatch = await user.matchPassword(password);
-        if (!isMatch) {
-            return res
-                .status(401)
-                .json({ success: false, error: 'Invalid credentials'});
-        }
+        // const isMatch = await user.matchPassword(password);
+        //
+        // if (!isMatch) {
+        //     return res
+        //         .status(401)
+        //         .json({ success: false, msg: 'Invalid credentials' });
+        // }
         res.status(200).json({ success: true, user: user });
     } catch (err) {
         return res
             .status(401)
             .json({
                 success: false,
-                error: err.message
+                msg: 'Cannot convert email or password to string'
             });
     }
 };
